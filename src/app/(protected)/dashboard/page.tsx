b34af56d8f87120d812fa5e20269c3b1435cb335
Aren <arenjinbachian@gmail.com>
2026-06-18 22:30:13 -0400

    add logout button testing and root route redirecting

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,7 +1,26 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { useAuth } from "@/context/AuthContext";
+
 const dashboard = () => {
-  return <div>Dashboard Page</div>;
+  const { logout } = useAuth();
+
+  const handleClick = async () => {
+    const response = await fetch("/api/auth/refreshToken/logout", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) logout();
+  };
+
+  return (
+    <>
+      <div>Dashboard Page</div>
+      <Button onClick={handleClick}>Logout</Button>
+    </>
+  );
 };
 
 export default dashboard;
